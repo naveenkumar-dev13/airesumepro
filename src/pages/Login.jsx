2ff64cba94d046f7bf8c4dd97e70a9df5526e3c7
naveenkumar-dev13 <naveenkumar.mfai@gmail.com>
2025-04-02 11:39:51 +0530

    Refactor layout in Login, ResumeAnalyzer, and SignUp components by adjusting max-width for improved responsiveness

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,7 +32,7 @@ function Login() {
       className="flex items-center justify-center h-screen  p-4 "
     >
       <div
-        className="flex  items-center justify-evenly gap-2 rounded-2xl  mx-auto max-w-6xl w-full max-md:p-3 "
+        className="flex  items-center justify-evenly gap-2 rounded-2xl  mx-auto max-w-5xl w-full max-md:p-3 "
         style={{ boxShadow: "0px 0px 10px 10px rgb(186, 213, 238,0.5)" }}
       >
         <form
